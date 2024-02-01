7de0c2392d9d97d3f9e2afd9507d3ebc2f3063e1
mrKisp <157525837+mrKisp@users.noreply.github.com>
2024-02-01 10:14:47 -0700

    Add files via upload

---
+++ Assignments/Assignment03/PriorityQueueTimer.java
@@ -0,0 +1,34 @@
+package assign03;
+
+public class PriorityQueueTimer extends TimerTemplate{
+
+	
+	public PriorityQueueTimer(int timesToLoop, int[] problemSizes) {
+		super(problemSizes, timesToLoop);
+	}
+
+	
+	protected void setup(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	protected void timingIteration(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	protected void compensationIteration(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
